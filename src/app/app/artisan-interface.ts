0b6fca127011407cd194f0d57702522d4ac5ae89
Mauricette-g <mauricettejoel17@gmail.com>
2025-01-17 10:26:51 +0100

    Delete src/app/app directory
    Doublon

--- src/app/app/artisan-interface.ts
+++
@@ -1,9 +0,0 @@
-
-export interface ArtisanInterface{
-    "id": number ,
-    "name": string ,
-    "artisanSpecialite" :string,
-    "photo" : string,
-    "artisanNote" : number,
-    "artisanLocalisation" : string
-}
